8a42df3a2bb54f5141890a994b9c9042c1ae67a6
William Torres <39351781+williamtorres1@users.noreply.github.com>
2020-04-04 02:55:50 +0800

    Update deepin.sh

--- deepin.sh
+++ deepin.sh
@@ -66,3 +66,5 @@ echo "export ANDROID_HOME=~/Android/Sdk" >> ./.bashrc
 echo "export PATH=$PATH:$ANDROID_HOME/tools" >> ./.bashrc
 echo "export PATH=$PATH:$ANDROID_HOME/platform-tools" >> ./.bashrc
 ~/Android/Sdk/tools/bin/sdkmanager "platform-tools" "platforms;android-27" "build-tools;27.0.3" -y
+
+reboot
