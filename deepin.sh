04cd93343d0bd60d710d912185718d2901136611
William Torres <39351781+williamtorres1@users.noreply.github.com>
2020-04-06 20:39:50 -0300

    Update deepin.sh

--- deepin.sh
+++ deepin.sh
@@ -27,6 +27,7 @@ sudo apt-get update
 sudo apt-get install -y nodejs
 sudo apt-get install --no-install-recommends yarn -y
 sudo apt-get install openjdk-8-jdk -y
+sudo apt-get install insomnia -y
 sudo apt-get install curl -y
 sudo apt-get install git -y
 sudo apt-get install scrcpy -y
@@ -42,11 +43,10 @@ sudo apt-get install kdenlive -y
 # Fazendo download de programas externos
 mkdir /home/${USER}/Downloads/softwares
 cd /home/${USER}/Downloads/softwares
-
-# chrome
-wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 # discord
 wget -c https://dl.discordapp.net/apps/linux/0.0.10/discord-0.0.10.deb
+# Insomnia
+wget -c https://github-production-release-asset-2e65be.s3.amazonaws.com/56899284/e217bd00-5296-11ea-98da-f03d5d4bdcc8?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20200406%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20200406T233856Z&X-Amz-Expires=300&X-Amz-Signature=a13dc558b352805217cf035a660736ba688ce406744e7cdbdf095177355ffc4c&X-Amz-SignedHeaders=host&actor_id=39351781&response-content-disposition=attachment%3B%20filename%3Dinsomnia_7.1.1_amd64.deb&response-content-type=application%2Foctet-stream
 sudo dpkg -i *.deb
 cd ..
 rm -Rf softwares
