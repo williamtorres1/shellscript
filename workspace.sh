e351fd6d99d4a97063dd35b1e262c6b0496d2b8b
William Torres <williamtorres074@gmail.com>
2020-09-01 15:57:14 -0300

    New script for my workspace for react native

---
+++ workspace.sh
@@ -0,0 +1,57 @@
+sudo apt-get install curl -y 
+echo
+echo "Instalando curl"
+echo
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+echo
+echo ">>> Adicionando repositório Node"
+echo
+sudo apt-get install -y nodejs
+echo
+node -v
+echo ">>> Versão do node"
+echo
+npm -v
+echo ">>> Versão do npm"
+echo
+
+echo ">>> Adicionando repositório Yarn"
+echo
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+echo 
+echo ">>> Adicionando repositório JDK 8"
+echo
+sudo add-apt-repository ppa:openjdk-r/ppa
+
+echo
+echo ">>> Atualizando repositórios"
+sudo apt-get update -y
+
+echo
+echo ">>> Instalando Yarn"
+echo 
+sudo apt install --no-install-recommends yarn -y
+echo
+yarn -v
+echo ">>> Versão do yarn"
+echo
+
+echo
+echo ">>> Instalando JDK 8"
+sudo apt-get install openjdk-8-jdk
+
+echo
+echo ">>> Versão do Java"
+java -version
+
+echo
+echo ">>> Instalando libs gráficas para emulação"
+echo
+sudo apt-get install gcc-multilib lib32z1 lib32stdc++6 -y
+
+echo
+echo ">>> Instalando KVM"
+echo
+sudo apt install qemu-kvm -y
